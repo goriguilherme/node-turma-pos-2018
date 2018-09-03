9b3232b4e6dcc4f76cc6e485ccff3ff912723c44
Guilherme Cesar Gori <guilhermecesargori@MacBook-Air-de-Guilherme.local>
2018-09-03 10:22:47 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+const tasks = [];
+
+app.post('/tasks', (request, response) => {
+
+    const body = request.body;
+    const task = {
+        id: Math.random().toString().replace('0.', ''),
+        title: body.title,
+        resume: body.resume,
+        isDone: body.isDone,
+        isPriority: body.isPriority
+    };
+
+    tasks.push(task);
+    response.status(201);
+    response.send(task);
+
+});
+
+app.get('/tasks', (request, response) => {
+    response.send(tasks);
+});
+
+app.get('/tasks/:taskId', (request, response) => {
+
+    const task = tasks.filter( t => t.id === request.params.taskId);
+    
+    if (task) {
+        response.send(task);
+    } else {
+        response.status(404);
+        response.send();
+    }
+
+});
+
+app.listen(3000, () => {
+    console.log('Server Running on port 3000');
+});
